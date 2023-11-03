3ced6d70c9a9ed002b517477c59441c008826b9b
Федоров Даниил <fedorov_danilka@bk.ru>
2023-11-03 17:47:01 +0500

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TeleWeb from '@/utils/teleWeb'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+
+  mounted() {
+    // let teleWeb = document.createElement('script');
+    // teleWeb.setAttribute('src', 'https://telegram.org/js/telegram-web-app.js');
+    // document.head.appendChild(teleWeb);
+    Telegram.WebApp.expand()
+    this.initData();
+  },
+
+  methods: {
+    initData() {
+      console.log(Telegram.WebApp.initDataUnsafe.user?.id);
+    }
   }
 }
 </script>
